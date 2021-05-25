136f8c631e126cc4a4d6566b20666077c7ab3595
antoine leleu <antoine-41@live.fr>
2021-05-25 17:55:34 +0200

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Test = "Coucou";
+            TempData["Caca"] = "CHocho";
+            Session["President"] = "Trouducul";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Test = "Coucou";
+            TempData["Caca"] = "CHocho";
+            Session["President"] = "Trouducul";
+            return RedirectToAction("Contact");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MyMessages()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyMessages(Message m)
+        {
+            ViewBag.Message = m;
+            return View();
+        }
+
+        public ActionResult MyListString()
+        {
+            ViewBag.Liste = new List<string> { "coucou", "caca", "jiji" };
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyListString(List<string> vals)
+        {
+            ViewBag.Liste = vals;
+            return View();
+        }
+
+        public ActionResult MyListMessages()
+        {
+            ViewBag.Liste = new List<string> { "coucou", "caca", "jiji" };
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyListMessages(List<Message> vals)
+        {
+            ViewBag.Liste = vals;
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Message
+    {
+        public string Emetteur { get; set; }
+        public string Contenu { get; set; }
+        public DateTime Date
+        { get; set; }
+    }
+}
